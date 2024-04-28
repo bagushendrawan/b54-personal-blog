8249fe47fb59fccd86f73bc37c1139b402e9696d
Baguser7 <bagushendrawan01@gmail.com>
2024-04-28 15:41:48 +0700

    database update

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,116 +1,4 @@
-//var dataProject = [];
 
-// function addProject(event) {
-//   event.preventDefault();
-
-//   let checkArr = [];
-//   let name = document.getElementById("name").value;
-//   let startdate = document.getElementById("start").value;
-//   let enddate = document.getElementById("end").value;
-//   let desc = document.getElementById("desc").value;
-//   let check = document.querySelectorAll("input[type=checkbox]:checked");
-//   let image = document.getElementById("file").files[0];
-//   let imageURL = URL.createObjectURL(image);
-
-//   if (name === "") {
-//     return alert("Please entered your name!");
-//   } else if (startdate === "") {
-//     return alert("Please entered your start date!");
-//   } else if (enddate === "") {
-//     return alert("Please entered your end date!");
-//   } else if (desc === "") {
-//     return alert("Please entered your description!");
-//   } else if (image === "") {
-//     return alert("Please entered your image!");
-//   }
-
-//   if(startdate > enddate)
-//   {
-//     return alert("Start date cannot started earlier than end date");
-//   }
-
-//   let timeDifferencesMs = new Date(enddate) - new Date(startdate);
-//   let differencesDay = Math.floor(timeDifferencesMs / (1000 * 60 * 60 * 24));
-//   let differenceMonth = Math.floor(differencesDay / 30.437);
-//   let differenceYear = Math.floor(differenceMonth / 12);
-
-//   let dateDifferences;
-//   if (differenceYear > 0) {
-//     dateDifferences = `${differenceYear} Year ${differenceMonth%12} Month ${differencesDay%30} Day`;
-//   } else if (differenceMonth > 0) {
-//     dateDifferences = `${differenceMonth%12} Month ${differencesDay%30} Day`;
-//   } else {
-//     dateDifferences = `${differencesDay%30} Day`;
-//   }
-
-//   for (var i = 0; i < check.length; i++) {
-//     checkArr.push(check[i].value);
-//   }
-
-//   dataProject.unshift({
-//     name: name,
-//     start: startdate,
-//     end: enddate,
-//     desc: desc,
-//     checkbox: checkArr,
-//     img: imageURL,
-//     duration: dateDifferences,
-//   });
-
-//   //console.log(enddate - startdate);
-//   //console.log(timeDifferencesMs);
-//   console.log(imageURL);
-//   console.log(dataProject);
-
-//   viewProject();
-// }
-
-// function viewProject(dataPro) {
-//   let html = "";
-//   dataPro.forEach((data) => {
-    
-//     let checkboxes = "";
-//     data.checkbox.forEach((check) => {
-//       switch (check) {
-//         case "node":
-//           checkboxes += `<img src="../assets/img/icon/node-js.png" style="height:1.5rem" />`;
-//           break;
-//         case "react":
-//           checkboxes += `<img src="../assets/img/icon/atom.png" style="height:1.5rem" />`;
-//           break;
-//         case "next":
-//           checkboxes += `<img src="../assets/img/icon/next-js-seeklogo.svg" style="height:1.5rem" />`;
-//           break;
-//         case "typeScript":
-//           checkboxes += `<img src="../assets/img/icon/typescript.png" style="height:1.5rem" />`;
-//           break;
-//       }
-//     });
-
-//     html += `
-//     <a href="./project-page.html" class="text-none text-dark">
-//           <div class="card p-2" style="width: 18rem;">
-//             <img src="${data.img}" class="card-img-top" alt="..." style="height: 200px; object-fit: cover;">
-//             <div class="card-body">
-//               <h5 class="card-title">${data.name}</h5>
-//               <p class="card-text" id="duration">${data.duration}</p>
-//               <p class="card-text"  id="syn">${data.desc}</p>
-//               <div class="icons mb-4">${checkboxes}</div>
-
-//               <div class="row g-2 mx-a d-flex justify-content-center">
-//                 <button href="#" class="col-5 btn btn-primary me-4 border-0 black">Edit</button>
-//                 <button href="#" class="col-5 btn btn-primary border-0 black">Delete</button>
-//               </div>
-              
-//               </div>
-//           </div>
-//     </a>`;
-
-
-//   });
-
-//   document.getElementById("gallery").innerHTML = html;
-// }
 
 async function deleteProject(id)
 {
@@ -127,30 +15,17 @@ async function editProject(id)
   const start = document.getElementById("start").value;
   const end = document.getElementById("end").value;
   const desc = document.getElementById("desc").value;
-  const node = document.querySelector('input[id="node"]:checked').value;
-  const next = document.querySelector('input[id="next"]:checked').value;
-  const react = document.querySelector('input[id="react"]:checked').value;
-  const ts = document.querySelector('input[id="ts"]:checked').value;
-  let icons = []
 
-  if(node)
-  {
-    icons.push(node);
+  let icons = []
+  var checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+
+  for (var i = 0; i < checkboxes.length; i++) {
+    icons.push(checkboxes[i].value)
   }
-  if(next)
-  {
-    icons.push(next);
-  }
-  if(react)
-  {
-    icons.push(react);
-  }
-  if(ts)
-  {
-    icons.push(ts);
-  }
+
 
   const obj = {
+    id: id,
     name: name,
     start: start,
     end: end,
@@ -158,16 +33,16 @@ async function editProject(id)
     iconsArray: icons
   }
 
-  // console.log("here");
-  // console.log(obj);
-
-  await fetch(`/edit-project/${id}`,
+ const response = fetch(`/patch-project`,
 		{
 			method: 'PATCH',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(obj)
+      body: JSON.stringify(obj),
+      cache: "no-cache",
+      redirect: "follow"
     });
+
 
 }
--- backend.js
+++ backend.js
@@ -1,216 +1,247 @@
 //const viewProject = require("./assets/js/project");
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 const app = express();
-const path = require('node:path');
+const path = require("node:path");
+const multer = require("multer");
+const fs = require('fs');
+const {Sequelize, QueryTypes} = require("sequelize");
+const { type } = require("node:os");
+const config = require('./config/config.json')
+const sequelize = new Sequelize(config.development);
 
 const port = 5000;
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/assets", express.static(path.join(__dirname, "./assets")));
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, "./bootstrap"));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./bootstrap"));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './assets/tmp/uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+  }
+});
+
+const upload = multer({ storage: storage });
 
 let dataProject = [];
 
-app.get('/', function (req, res) {
-    res.render("index");
-  });
+// -------------------------------------------------------------------------
 
-app.get('/contact-form', function (req, res) {
-    res.render("contact-form");
-  });
+async function fetchBlog (req, res) {
+  const query = 'SELECT * FROM blogs';
+  const obj = await sequelize.query(query, {type: QueryTypes.SELECT});
+  console.log(obj);
+  const { id, name, start, end, desc, file, iconsArray } = obj[0];
+  const duration = calculateDuration(start, end);
+  const iconsPath = iterateIcons(iconsArray);
+  console.log("Iconspath " + iconsPath);
+  console.log("Duration " + duration);
+  res.render("index", {obj: obj, duration: duration, iconsPath: iconsPath});
+}
 
-app.get('/new-project', function (req, res) {
-    res.render("new-project", {dataProject});
-  });
+//Render home page
+app.get("/", (req, res) => {
+  fetchBlog(req, res);
+});
 
-app.get('/project-page:id', function (req, res) {
+//Render contact form page
+app.get("/contact-form", function (req, res) {
+  res.render("contact-form");
+});
+
+//Render add project page
+app.get("/new-project", function (req, res) {
+  res.render("new-project");
+});
+
+// Open page details
+app.get("/project-page:id", function (req, res) {
   const id = parseInt(req.params.id);
-  const dataArr = getItemById(dataProject,id);
-  const data = dataArr[0];;
-  res.render("project-page", {data});
-  });
+  const dataArr = getItemById(dataProject, id);
+  const data = dataArr[0];
+  res.render("project-page", { data });
+});
 
-app.post('/new-project', function (req, res) {
-    const {name, start, end, desc, file, icons } = req.body;
+// Open the edit page for selected blog
+app.get("/edit-project:id", function (req, res) {
+  const { id } = req.params;
+  const dataArr = getItemById(dataProject, id);
+  const data = dataArr[0];
+  let iconIdAray = {};
 
-    const duration = calculateDuration(start, end);
-    const iconsArray = iterateIcons(icons);
-    const length = dataProject.length;
-    //console.log("length" + length);
-    dataProject.unshift({
-        id: length,
-        name,
-        start,
-        end,
-        duration,
-        desc,
-        file,
-        iconsArray
-    })
-    //console.log("Data has been added!");
-    console.log(dataProject);
-    res.redirect("new-project");
-  });
-
-app.get('/edit-project:id', function (req, res) {
-    const {id } = req.params;
-    const dataArr = getItemById(dataProject,parseInt(id));
-    const data = dataArr[0];;
-    console.log("dataArr" + dataArr);
-    let iconIdAray = {};
-
-    // console.log(data);
-    // console.log(data.iconsArray);
-    // console.log(typeof(data.iconsArray));
-
-    if(data.iconsArray !== undefined)
-    {
-      data.iconsArray.forEach((val) => {
-        switch(val) {
-          case "../assets/img/icon/node-js.png":
-            iconIdAray.node = true;
+  if (data.iconsArray !== undefined) {
+    data.iconsArray.forEach((val) => {
+      switch (val) {
+        case "../assets/img/icon/node-js.png":
+          iconIdAray.node = true;
           break;
-          case "../assets/img/icon/atom.png":
-            iconIdAray.react = true;
+        case "../assets/img/icon/atom.png":
+          iconIdAray.react = true;
           break;
-          case "../assets/img/icon/next-js-seeklogo.svg":
-            iconIdAray.next = true;
+        case "../assets/img/icon/next-js-seeklogo.svg":
+          iconIdAray.next = true;
           break;
-          case "../assets/img/icon/typescript.png":
-            iconIdAray.ts = true;
+        case "../assets/img/icon/typescript.png":
+          iconIdAray.ts = true;
           break;
-          default:
-            console.log("Check the iconsArray");
-            break;
-        }
-      })
-    }
+        default:
+          console.log("Check the iconsArray");
+          break;
+      }
+    });
+  }
 
-    data["iconIdArray"] = iconIdAray;
-    // console.log(data["iconIdArray"]);
-    // console.log(data["iconIdArray"].next);
-    res.render("edit-project", {data});
-  });
+  data["iconIdArray"] = iconIdAray;
+  res.render("edit-project", { data });
+});
 
-app.patch('/edit-project:id',function (req, res) {
-    const {id } = req.params;
-    const dataPatch = req.body;
-    //console.log("here i come");
-    //console.log(dataPatch);
-    dataProject.splice(parseInt(id)-1, 1, dataPatch);
-    res.redirect(301,"new-project");
-  });
+// ----------------------------------------------------------------------
+// Multer upload file
+app.post("/upload-file", upload.single('file'), function (req, res) {
+  console.log("HIT");
+  console.log(req.file);
+  res.sendStatus(200);
+  res.send(req.file);
+});
 
-app.post('/edit-project:id',function (req, res) {
-    const {id } = req.params;
-    const {name, start, end, desc, file, icons} = req.body;
+// Redirect after edit blog
+app.post("/confirm-edit", function (req, res) {
+  res.render("index", { dataProject });
+});
 
-    const duration = calculateDuration(start, end);
-    const iconsArray = iterateIcons(icons);
-
-    const data = {
-      id: parseInt(id),
-      name,
-      start,
-      end,
-      duration,
-      desc,
-      file,
-      iconsArray
-    }
-    //console.log("here i come");
-    //console.log(icons);
-    //console.log("id" + id);
-    dataProject.splice(getindexbyID(dataProject,id), 1, data);
-    //console.log(dataProject);
-    res.render("new-project", {dataProject});
-  });
+// Create new blog
+app.post("/new-project", upload.single('file'), function (req, res) {
+  console.log("New Post Created!");
   
+  // console.log(req.file);
+  const { name, start, end, desc, icons } = req.body;
 
-app.delete('/new-project/:id', function (req, res) {
-    const {id } = req.params;
-    dataProject = deleteItemById(dataProject, parseInt(id));
-    //console.log(dataProject);
-    //res.redirect("new-project");
+  const duration = calculateDuration(start, end);
+  const iconsArray = iterateIcons(icons);
+  const length = dataProject.length;
+  let filePath;
+  if(req.file) filePath = req.file.path;
+  dataProject.unshift({
+    id: length,
+    name,
+    start,
+    end,
+    duration,
+    desc,
+    file: filePath,
+    iconsArray,
   });
 
+  // console.log(dataProject);
+  // console.log(dataProject[0].file);
+  res.redirect("/");
+});
 
-  
+//Edit blog before redirect
+app.patch("/patch-project", upload.single('file'), function (req, res) {
+  const { id, name, start, end, desc, file, iconsArray } = req.body;
+  const duration = calculateDuration(start, end);
+  const iconsPathArray = iterateIcons(iconsArray);
+  let filePath;
+  if(req.file) filePath = req.file.path;
+  const data = {
+    id,
+    name,
+    start,
+    end,
+    duration,
+    desc,
+    file: filePath,
+    iconsArray: iconsPathArray,
+  };
+
+  dataProject.splice(getindexbyID(dataProject, id), 1, data);
+});
+
+//Delete Blog
+app.delete("/new-project/:id", function (req, res) {
+  const { id } = req.params;
+
+  fs.unlinkSync(dataProject[id].file);
+  dataProject = deleteItemById(dataProject, id);
+});
+
 app.listen(port);
 
 function calculateDuration(start, end) {
-    let timeDifferencesMs = new Date(end) - new Date(start);
-    let differencesDay = Math.floor(timeDifferencesMs / (1000 * 60 * 60 * 24));
-    let differenceMonth = Math.floor(differencesDay / 30.437);
-    let differenceYear = Math.floor(differenceMonth / 12);
-  
-    let dateDifferences;
-    if (differenceYear > 0) {
-      dateDifferences = `${differenceYear} Year ${differenceMonth%12} Month ${differencesDay%30} Day`;
-    } else if (differenceMonth > 0) {
-      dateDifferences = `${differenceMonth%12} Month ${differencesDay%30} Day`;
-    } else {
-      dateDifferences = `${differencesDay%30} Day`;
-    }
+  let timeDifferencesMs = new Date(end) - new Date(start);
+  let differencesDay = Math.floor(timeDifferencesMs / (1000 * 60 * 60 * 24));
+  let differenceMonth = Math.floor(differencesDay / 30.437);
+  let differenceYear = Math.floor(differenceMonth / 12);
 
-    return dateDifferences;
+  let dateDifferences;
+  if (differenceYear > 0) {
+    dateDifferences = `${differenceYear} Year ${differenceMonth % 12} Month ${
+      differencesDay % 30
+    } Day`;
+  } else if (differenceMonth > 0) {
+    dateDifferences = `${differenceMonth % 12} Month ${
+      differencesDay % 30
+    } Day`;
+  } else {
+    dateDifferences = `${differencesDay % 30} Day`;
+  }
+
+  return dateDifferences;
 }
 
-function iterateIcons(arr)
-{
-    if (arr) {
-        let checkboxes = arr.map((check) => {
-            switch (check) {
-                case "node":
-                    return "../assets/img/icon/node-js.png";
-                case "react":
-                    return "../assets/img/icon/atom.png";
-                case "next":
-                    return "../assets/img/icon/next-js-seeklogo.svg";
-                case "typeScript":
-                    return "../assets/img/icon/typescript.png";
-                default:
-                    return null;
-            }
-        });
+function iterateIcons(arr) {
+  if (arr) {
+    let checkboxes = arr.map((check) => {
+      console.log(check);
+      switch (check) {
+        case "node":
+          return "../assets/img/icon/node-js.png";
+        case "react":
+          return "../assets/img/icon/atom.png";
+        case "next":
+          return "../assets/img/icon/next-js-seeklogo.svg";
+        case "typeScript":
+          return "../assets/img/icon/typescript.png";
+        default:
+          return null;
+      }
+    });
 
-        return checkboxes;
-    }
+    return checkboxes;
+  }
 }
 
-function deleteItemById(obj,id) {
+function deleteItemById(obj, id) {
   obj = obj.filter((item) => {
-    //console.log(typeof(item.id) + "/" + typeof(id));
-    return item.id !== id
+    return item.id != id;
   });
   
-  //console.log("sucessfully deleted!");
   return obj;
 }
 
-function getItemById(obj,id) {
+function getItemById(obj, id) {
   obj = obj.filter((item) => {
-    return item.id === id
+    return item.id == id;
   });
-  
-  //console.log("sucessfully deleted!");
-  //console.log("-------" + JSON.stringify(obj));
+
   return obj;
 }
 
-function getindexbyID(obj,id)
-{
- const index = obj.filter((val, ind) => {
-    if(val.id === id)
-    {
-      return ind
+function getindexbyID(obj, id) {
+  const index = obj.filter((val, ind) => {
+    if (val.id == id) {
+      return ind;
     }
-  })
-
+  });
   return index;
 }
